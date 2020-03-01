126e706a06538d3b94f8306aa4b6b678e7bcfb99
rtisma <942951+rtisma@users.noreply.github.com>
2020-03-01 11:07:35 -0500

    Release Candidate 1.1.0

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-MY_UID=$(id -u) MY_GID=$(id -g) docker-compose build
-MY_UID=$(id -u) MY_GID=$(id -g) docker-compose up -d
+postgresql_version=$(cat  ./POSTGRESQL_VERSION)
+MY_UID=$(id -u) MY_GID=$(id -g) POSTGRESQL_VERSION=${postgresql_version} docker-compose build
+MY_UID=$(id -u) MY_GID=$(id -g) POSTGRESQL_VERSION=${postgresql_version} docker-compose up -d
